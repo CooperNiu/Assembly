091331a143fcbfbd600c0cc8f32a335206865179
Mo Jiajun <767089873@qq.com>
2016-12-22 18:08:28 +0800

    Update 12.21_1_02_out.asm

--- 12.21_1_02_out.asm
+++ 12.21_1_02_out.asm
@@ -3,7 +3,7 @@
 ;***************************************************
 ls273  equ 2a8h      ;EQU语句给符合定义一个值，或者其他符号名，此处赋值地址,ls273 只是一个名字，可以改变     
 data segment
-    str db "press esc to exit...,press other key to output in the screen$";提示按任何键导致退出
+    
 data ends   
 
 stack segment stack
@@ -12,14 +12,7 @@ stack ends
 
 code segment 'code'
  assume  cs:code,ds:data,ss:stack
-start: mov ax,data
-       mov ds,ax
-       mov es,ax
-       mov dx,offset str  ;取偏移地址
-       mov ah,9      ;课本137页，字符串显示，调用号为ah=09h  ds:dx,此寄存器对指向内存中的字符串的首址，
-       int 21h       ;此功能调用显示字符直到遇到终止字符串的$字符
-      
-L1:    mov ah,2      ;调用号ah=02h，显示输出，DL=要显示的ASCII字符代码
+start: mov ah,2      ;调用号ah=02h，显示输出，DL=要显示的ASCII字符代码
        mov dl,0dh    ;显示回车符 0dh=00001101B ，查表 为CR，即为回车
        int 21h
        mov ah,1      ;调用号ah=01h,读键盘字符并回显，出口参数AL=读到的键盘字符
